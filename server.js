b45ee3cd4b5f6cc3c1b45516b0047c11f00b6822
katunguTECH <katungu1@gmail.com>
2026-08-01 12:59:59 +0300

    Fix duplicate mongooseOptions declaration

--- server.js
+++ server.js
@@ -46,7 +46,6 @@ if (!MONGODB_URI) {
 let isMongoConnected = false;
 
 const mongooseOptions = {
-const mongooseOptions = {
   serverSelectionTimeoutMS: 30000,
   socketTimeoutMS: 45000,
   family: 4,
